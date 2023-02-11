393d788ee2b3dee64237d1fbb41cc20cb516168a
devIS <ivan.shaptalov1@gmail.com>
2023-02-11 10:19:02 +0200

    init(lucid_bell): lucid bell repository initialized;

---
+++ android/app/src/main/kotlin/com/example/flutter_lucid_bell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_lucid_bell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bell_logic.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BellLogic extends StatefulWidget {
+  const BellLogic({super.key});
+
+  @override
+  State<BellLogic> createState() => _BellLogicState();
+}
+
+class _BellLogicState extends State<BellLogic> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
