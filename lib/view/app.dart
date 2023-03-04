362b58949fb339b406a3a71ab6eaf9666b8b6e8a
devIS <ivan.shaptalov1@gmail.com>
2023-03-04 10:22:34 +0200

    feat(): added black system overlay;

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -24,66 +24,76 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-     SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     // ignore: invalid_use_of_protected_member
     final List<Widget> pages = [
       HomeScreen(updateCallback),
       const PermissionPage()
     ];
-    return MaterialApp(
-        title: 'Lucid Bell Simplest',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeSetting.loadCalmTheme(),
-        home: BellPresenter.showFeaturesPage
-            ? WelcomeScreen(updateCallback)
-            : Container(
-                decoration: BoxDecoration(
-                    gradient: BellPresenter.isBellRunning() // active
-                        ? const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight, // #380036 > #0CBABA
-                            colors: [
-                                Color.fromARGB(197, 66, 119, 253),
-                                Color.fromARGB(180, 15, 4, 11)
-                              ])
-                        : const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight, // #380036 > #0CBABA
-                            colors: [
-                                Color.fromARGB(199, 88, 37, 147),
-                                Color.fromARGB(91, 21, 14, 25)
-                              ])),
-                child: Scaffold(
-                  backgroundColor: Colors.transparent,
-                  body: pages.elementAt(currentPage),
-                  bottomNavigationBar: BottomNavigationBar(
-                      backgroundColor: Colors.transparent,
-                      items: const [
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.home),
-                            label: "home",
-                            backgroundColor: Colors.transparent),
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.settings),
-                            label: "settings",
-                            backgroundColor: Colors.transparent),
-                      ],
-                      currentIndex: currentPage,
-                      unselectedItemColor: Colors.grey,
-                      fixedColor: BellPresenter.isBellRunning()
-                          ? Colors.green
-                          : Colors.deepPurple,
-                      onTap: (int inIndex) {
-                        setState(() {
-                          BellPresenter.clearCallbackTriggers();
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent, //top status bar
+        systemNavigationBarColor:
+            Colors.black, // navigation bar color, the one Im looking for
+        statusBarIconBrightness: Brightness.dark, // status bar icons' color
+        systemNavigationBarIconBrightness:
+            Brightness.dark, //navigation bar icons' color
+      ),
+      child: MaterialApp(
+          title: 'Lucid Bell Simplest',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeSetting.loadCalmTheme(),
+          home: BellPresenter.showFeaturesPage
+              ? WelcomeScreen(updateCallback)
+              : Container(
+                  decoration: BoxDecoration(
+                      gradient: BellPresenter.isBellRunning() // active
+                          ? const LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight, // #380036 > #0CBABA
+                              colors: [
+                                  Color.fromARGB(197, 66, 119, 253),
+                                  Color.fromARGB(180, 15, 4, 11)
+                                ])
+                          : const LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight, // #380036 > #0CBABA
+                              colors: [
+                                  Color.fromARGB(199, 88, 37, 147),
+                                  Color.fromARGB(91, 21, 14, 25)
+                                ])),
+                  child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    body: pages.elementAt(currentPage),
+                    bottomNavigationBar: BottomNavigationBar(
+                        backgroundColor: Colors.transparent,
+                        items: const [
+                          BottomNavigationBarItem(
+                              icon: Icon(Icons.home),
+                              label: "home",
+                              backgroundColor: Colors.transparent),
+                          BottomNavigationBarItem(
+                              icon: Icon(Icons.settings),
+                              label: "settings",
+                              backgroundColor: Colors.transparent),
+                        ],
+                        currentIndex: currentPage,
+                        unselectedItemColor: Colors.grey,
+                        fixedColor: BellPresenter.isBellRunning()
+                            ? Colors.green
+                            : Colors.deepPurple,
+                        onTap: (int inIndex) {
+                          setState(() {
+                            BellPresenter.clearCallbackTriggers();
 
-                          currentPage = inIndex;
-                        });
-                      }),
-                ),
-              ));
+                            currentPage = inIndex;
+                          });
+                        }),
+                  ),
+                )),
+    );
   }
 }
