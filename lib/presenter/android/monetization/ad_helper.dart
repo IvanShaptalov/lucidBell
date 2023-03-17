e8bf4e7236dcf65549e4fc310d66293b3562cbee
devIS <ivan.shaptalov1@gmail.com>
2023-03-17 16:01:57 +0200

    feat(): admob id added

--- lib/presenter/android/monetization/ad_helper.dart
+++ lib/presenter/android/monetization/ad_helper.dart
@@ -11,32 +11,14 @@ class AdHelper {
     return ConfigAd.showAds;
   }
 
-  static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      throw UnsupportedError('Unsupported platform');
-    }
-  }
 
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4411468910";
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
-  }
 
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/5224354917";
-    } else if (Platform.isIOS) {
+      return "ca-app-pub-4618505570484622/7719749288";
+    } /* else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/1712485313";
-    } else {
+    } */ else {
       throw UnsupportedError("Unsupported platform");
     }
   }
