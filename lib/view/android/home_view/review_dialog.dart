bc483d4ad304fb2e769e48b13d3267076d7b7f0a
devIS <ivan.shaptalov1@gmail.com>
2023-03-21 15:49:02 +0200

    feat(): small fix #1

--- lib/view/android/home_view/review_dialog.dart
+++ lib/view/android/home_view/review_dialog.dart
@@ -5,6 +5,7 @@ import 'package:flutter_lucid_bell/presenter/android/in_app_review.dart';
 
 class ReviewDialog {
   static void showReviewDialog(context) {
+    
     var value = Random().nextInt(10);
     if (kDebugMode) {
       print(value);
